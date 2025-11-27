2df633608cfd2fe1133f622d313c95a8931ff30c
ottlseo <dotsi@ewhain.net>
2025-11-27 12:48:05 +0900

    update basic.py

--- 1-basic-agent/completed/basic.py
+++ 1-basic-agent/completed/basic.py
@@ -1,7 +1,5 @@
 from strands import Agent
-from strands_tools import calculator, current_time, use_aws, python_repl # 참고: https://github.com/strands-agents/tools
+from strands_tools import calculator, current_time, python_repl # 참고: https://github.com/strands-agents/tools
 
-agent = Agent(tools=[calculator, current_time, use_aws, python_repl]) # tools
-response = agent("What is 80/4?") # prompt
-
-print(response)
+agent = Agent(tools=[calculator, current_time, python_repl]) # tools
+response = agent("80 / 4 * 5 의 제곱근은?") # prompt
