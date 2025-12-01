b69e0aef9f2ecf8004bd0a20583ed68afc61cf79
ottlseo <dotsi@ewhain.net>
2025-12-01 11:34:05 +0900

    fix

--- 1-basic-agent/completed/custom_tool2.py
+++ 1-basic-agent/completed/custom_tool2.py
@@ -7,6 +7,9 @@ agent = Agent(
 
 if __name__ == "__main__":
     user_input = "Hello world를 출력하는 파이썬 코드를 작성하고 실행시켜줄래?"
-    # user_input = "completed 폴더에 무슨 파일 있는지 확인해줘"
+    
+    ## 또는, 아래 주석을 해제하여 프롬프트를 바꿔서 실행해보세요
+    # user_input = "1-basic-agent/completed 폴더에 무슨 파일 있는지 확인해줘"
 
-    response = agent(user_input) 
+    response = agent(user_input)
+
